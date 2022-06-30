27342a2e66425efaa5b6c4e7a1b7a521f435d06d
Luan Fabri <107646613+luannzin@users.noreply.github.com>
2022-06-30 20:18:14 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+const intro = document.querySelector('.intro');
+const distanceScroll = document.querySelectorAll('.p_content p');
+const windowMetade = window.innerHeight * 0.6;
+
+function animaScroll() {
+  if (pageYOffset > 500) {
+    distanceScroll.forEach((p) => {
+      const scrollTop = p.getBoundingClientRect().top - windowMetade;
+
+      console.log(scrollTop);
+
+      if (scrollTop < 100) {
+        p.classList.add('ativo');
+      }
+    });
+  }
+}
+
+window.addEventListener('scroll', animaScroll);
+
+window.addEventListener('scroll', () => {
+  if (pageYOffset > 0) {
+    intro.classList.add('ativo');
+  } else if (pageYOffset <= 0) {
+    intro.classList.remove('ativo');
+  }
+});
